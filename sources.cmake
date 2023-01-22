94457402fe908ea823e783a2a89f558ce512b82f
Yurii Cherkasov <strategarius@protonmail.com>
2023-01-22 16:02:28 +0700

    Windows-specific smart pointers

--- sources.cmake
+++ sources.cmake
@@ -18,29 +18,30 @@ set(WINAPI_HELPERS_CPP
 )
 
 set(WINAPI_HELPERS_H
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/utilities.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/bios.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/co_initializer.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/dynamic_handler_map.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/md5.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/win_errors.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/handle_ptr.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/hardware_information.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/native_api_helper.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/one_instance.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/partition_information.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/win_partition_information.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/physical_memory.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/process_helper.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/ptrs.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/registry_helper.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/service_helper.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/special_path_helper.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/win_special_path_helper.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/user_information.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/system_information.h
-	${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/win_user_information.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/bios.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/co_initializer.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/dynamic_handler_map.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/handle_ptr.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/hardware_information.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/md5.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/native_api_helper.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/one_instance.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/partition_information.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/physical_memory.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/process_helper.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/registry_helper.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/service_helper.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/special_path_helper.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/system_information.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/user_information.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/utilities.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/win_errors.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/win_partition_information.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/win_ptrs.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/win_special_path_helper.h
+  ${CMAKE_CURRENT_SOURCE_DIR}/include/winapi-helpers/win_user_information.h
 )
+
 set(WINAPI_HELPERS_SOURCES 
 	${WINAPI_HELPERS_CPP} 
 	${WINAPI_HELPERS_H}
